2b964dfa42327140adccc7249502f965550472f0
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-08-13 20:54:34 -0700

    add unit::Rad and unit::Deg angle measurement types

--- src/unit/mod.rs
+++ src/unit/mod.rs
@@ -2,8 +2,12 @@ mod mm;
 mod cm;
 mod m;
 mod km;
+mod rad;
+mod deg;
 
 pub use self::mm::*;
 pub use self::cm::*;
 pub use self::m::*;
 pub use self::km::*;
+pub use self::rad::{Rad, ToRad};
+pub use self::deg::{Deg, ToDeg};

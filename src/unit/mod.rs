9106fc0d6695ad798a81fd1a1affe66bf3826416
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-09-13 13:27:11 -0700

    Add workaround for rust-lang/rust#28394

--- src/unit/mod.rs
+++ src/unit/mod.rs
@@ -1,9 +1,10 @@
-mod mm;
-mod cm;
-mod m;
-mod km;
-mod rad;
-mod deg;
+// TODO: these must be pub because of Rust issue #28394
+pub mod mm;
+pub mod cm;
+pub mod m;
+pub mod km;
+pub mod rad;
+pub mod deg;
 
 pub use self::mm::*;
 pub use self::cm::*;
